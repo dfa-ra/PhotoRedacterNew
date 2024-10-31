d43d55a6fbb780075132ca8be796c5625402ff0e
ra <89213748002t@gmail.com>
2024-10-31 17:46:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/photoredacternew/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.photoredacternew;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.photoredacternew.databinding.ActivityMainBinding;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private final String url = "https://kinopoiskapiunofficial.tech/images/posters/kp_small/301.jpg";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> {
+
+            PhotoDialog dialog = PhotoDialog.getInstance(MainActivity.this);
+            dialog.open();
+
+            Picasso.get().load(url).into(new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    Log.e("aa99", "onBitmapLoaded");
+                    dialog.drawPhoto(new BitmapDrawable(getResources(), bitmap));
+                }
+
+                @Override
+                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                    Log.e("aa99", "onBitmapFailed: " + e.toString());
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+                    Log.d("aa99", "onPrepareLoad");
+                }
+            });
+        });
+    }
+}
---
+++ app/src/main/java/com/example/photoredacternew/PhotoDialog.java
@@ -0,0 +1,90 @@
+package com.example.photoredacternew;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+
+import com.example.photoredacternew.databinding.DialogPhotoViewBinding;
+
+/**
+ * далог для рассматривания изображений
+ */
+
+public class PhotoDialog extends Dialog {
+
+    private DialogPhotoViewBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private PhotoDialog(@NonNull Context context) {
+        super(context, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+
+        // во весь экран
+        getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+
+        // надуть байндинг
+        this.binding = DialogPhotoViewBinding.inflate(LayoutInflater.from(getContext()));
+        this.setContentView(binding.getRoot());
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        //попрятать навигацию и все лишнее
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        super.show();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        this.getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+        this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+
+        // отобразить загрузку
+        this.binding.loadAnimation.setVisibility(View.VISIBLE);
+
+        // закрыть диалог
+        this.binding.destroy.setOnClickListener(clc -> this.dismiss());
+
+    }
+
+    public void open () {
+        this.show();
+    }
+
+
+    // отрисовать изображение
+    public void drawPhoto(BitmapDrawable photo) {
+        Log.d("aa99", "start drawPhoto");
+        // спрятать загрузку
+        this.binding.loadAnimation.setVisibility(View.GONE);
+
+        // отрисовать фотку
+        this.binding.fullImageView.setVisibility(View.VISIBLE);
+        this.binding.fullImageView.setImageDrawable(photo);
+
+
+        Log.d("aa99", "end drawPhoto");
+    }
+
+    // билдер
+    public static PhotoDialog getInstance(Context context){
+        return new PhotoDialog(context);
+    }
+
+
+}
